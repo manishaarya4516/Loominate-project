c226442d80c84799519aa497d2a22db8e405b961
manishaarya4516 <manishaarya4516@gmail.com>
2023-07-16 21:53:18 -0700

    updatedd

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Dropdown, Space } from "antd";
 import {
   CommentOutlined,
@@ -12,23 +12,19 @@ import AddPostModal from "../add-post/add-post";
 import axios from "axios";
 
 const Feed = () => {
-  const[contentData,setContentData]= useState(null)
+  const [contentData, setContentData] = useState(null);
 
+  useEffect(() => {
+    axios
+      .get("https://loominatebackend-manishaarya4516.b4a.run/api/content/data")
+      .then(async (response) => {
+        const data = await response.data;
+        console.log(data);
+        setContentData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  useEffect(()=>{
-
-    axios.get("https://loominatebackend-manishaarya4516.b4a.run/api/content/data").then(async response =>{
-      const data = await response.data;
-      console.log(data);
-      setContentData(data)
-     
-    }).catch(err=>console.log(err))
-    
-
-  },[])
-
- 
-  
   console.log();
   return (
     <div>
@@ -55,104 +51,97 @@ const Feed = () => {
           </Dropdown>
         </p>
       </div>
-      {contentData ? contentData.map(data=>{
-          return(
-        <>
-            <div className="post-container">
-
-        
-      
-<div className="head-stack">
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-    }}
-    className="top"
-  >
-    <button>catagories</button>
-    <p>...</p>
-  </div>
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "start",
-      alignItems: "center",
-      gap: "10px",
-    }}
-    className="head"
-  >
-    <img
-      className="avatar-head"
-      src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
-      alt=""
-    />
-    <p>manish</p>
-  </div>
-  <div className="content">
-    <h3>{data.title}</h3>
-    <p>Description</p>
-    <p style={{ color: "red" }}>#tags</p>
-    <img
-      src={data.image}
-      alt=""
-    />
-  </div>
-</div>
-<div className="body-stack">
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "start",
-      alignItems: "center",
-      gap: "10px",
-    }}
-  >
-    <LikeFilled />
-    <p>0</p>
-    <DislikeFilled />
-    <p>0</p>
-    <CommentOutlined />
-    <p>0</p>
-  </div>
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "start",
-      alignItems: "center",
-      gap: "10px",
-    }}
-  >
-    <img
-      className="avatar-head"
-      src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
-      alt=""
-    />
-    <div className="comment-box">
-      <input type="text" className="comment-input" />
-      <button>
-        Post <SendOutlined />
-      </button>
-    </div>
-  </div>
-  <p
-    style={{
-      margin: "20px 0px 0px 0px ",
-      fontSize: "14px",
-      color: "blue",
-    }}
-  >
-    View all comments
-  </p>
-</div>
-</div>
-        </>
-           
-          )
-        }) :""}
-       
-  
+      {contentData
+        ? contentData.map((data) => {
+            return (
+              <>
+                <div className="post-container">
+                  <div className="head-stack">
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                      className="top"
+                    >
+                      <button>catagories</button>
+                      <p>...</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "start",
+                        alignItems: "center",
+                        gap: "10px",
+                      }}
+                      className="head"
+                    >
+                      <img
+                        className="avatar-head"
+                        src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
+                        alt=""
+                      />
+                      <p>manish</p>
+                    </div>
+                    <div className="content">
+                      <h3>{data.title}</h3>
+                      <p>Description</p>
+                      <p style={{ color: "red" }}>#tags</p>
+                      <img src={data.image} alt="" />
+                    </div>
+                  </div>
+                  <div className="body-stack">
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "start",
+                        alignItems: "center",
+                        gap: "10px",
+                      }}
+                    >
+                      <LikeFilled />
+                      <p>0</p>
+                      <DislikeFilled />
+                      <p>0</p>
+                      <CommentOutlined />
+                      <p>0</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "start",
+                        alignItems: "center",
+                        gap: "10px",
+                      }}
+                    >
+                      <img
+                        className="avatar-head"
+                        src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
+                        alt=""
+                      />
+                      <div className="comment-box">
+                        <input type="text" className="comment-input" />
+                        <button>
+                          Post <SendOutlined />
+                        </button>
+                      </div>
+                    </div>
+                    <p
+                      style={{
+                        margin: "20px 0px 0px 0px ",
+                        fontSize: "14px",
+                        color: "blue",
+                      }}
+                    >
+                      View all comments
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        : ""}
     </div>
   );
 };
